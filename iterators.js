9591c66d78b86be56ad8605766304221d13ce4c0
dohaisalive <dohaisalive@gmail.com>
2022-03-22 18:20:31 +0300

    TASK

--- iterators.js
+++ iterators.js
@@ -5,7 +5,7 @@
  * - Logs every element of the array
  ************************************/
 const logger = function (array) {
-
+    array.forEach(element => {console.log(element);})
 };
 
 /*************************************
@@ -20,7 +20,7 @@ const logger = function (array) {
  *   C = (F - 32) * (5/9)
  ************************************/
 const toCelsius = function (temperatures) {
-
+    return temperatures.map(element => element = (element - 32) * (5/9));
 };
 
 /**************************************
@@ -32,7 +32,7 @@ const toCelsius = function (temperatures) {
  *   that exceed the threshhold
  ***************************************/
 const hottestDays = function (temperatures, threshhold) {
-
+    return temperatures.filter(element => element>threshhold);
 };
 
 /******************************************
@@ -50,14 +50,16 @@ const hottestDays = function (temperatures, threshhold) {
  *       all previous functions
  *******************************************/
 const logHottestDays = function (temperatures, threshhold) {
-
+    let days = hottestDays(temperatures, threshhold);
+    let newTemp = toCelsius(days);
+    logger(newTemp);
 };
 
 /* Uncomment the following lines to test your code... */
 
 // logger([1, 2, 3, 4, 5, 6, 7]);
-// console.log(toCelsius([212, 122])); // should be: [100, 50];
-// console.log(hottestDays([1, 2, 3, 4, 5, 6, 7, 8, 13, 156, 1765], 5)); // should be : [6, 7, 8, 13, 156, 1765]
-// logHottestDays([140, 68, 212, 45, 149, 122, 19], 80); // should log "60", "100", "65", and "50"
+//console.log(toCelsius([212, 122])); // should be: [100, 50];
+//console.log(hottestDays([1, 2, 3, 4, 5, 6, 7, 8, 13, 156, 1765], 5)); // should be : [6, 7, 8, 13, 156, 1765]
+//logHottestDays([140, 68, 212, 45, 149, 122, 19], 80); // should log "60", "100", "65", and "50"
 
 module.exports = { logger, toCelsius, hottestDays, logHottestDays };
